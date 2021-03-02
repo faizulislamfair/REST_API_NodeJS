0fcc73c65070696393455431019488000d154a0e
S.M. Faizul Islam Fair <faizulislamfair@gmail.com>
2021-03-02 21:57:28 +0600

    Server Running

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+
+
+// set up express app
+const app = express();
+
+app.get('/api', function(req, res) {
+     console.log('GET request');
+     res.send({ name: 'Fair' });
+});
+
+// listen for requests
+app.listen(process.env.port || 4000, function(){
+     console.log('now listening for requests');
+});
+
