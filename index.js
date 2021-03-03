d0ff16d400e69456de67251c3f3cbe3384230708
S.M. Faizul Islam Fair <faizulislamfair@gmail.com>
2021-03-04 03:00:48 +0600

    Creating Routes

--- index.js
+++ index.js
@@ -1,13 +1,11 @@
 const express = require('express');
 
-
 // set up express app
 const app = express();
 
-app.get('/api', function(req, res) {
-     console.log('GET request');
-     res.send({ name: 'Fair' });
-});
+// initialize routes
+app.use('/api', require('./routes/api'));
+
 
 // listen for requests
 app.listen(process.env.port || 4000, function(){
